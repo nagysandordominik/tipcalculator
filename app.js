b8f12851fa92a16f02b0b542211b819870bd34cb
nagysandordominik <nagysandordominik@icloud.com>
2021-07-21 22:16:15 +0200

    finished here for today

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// const whichTip = document.querySelector('.tips');
-// whichTip.addEventListener("click", selectedTip);
+const whichTip = document.querySelector('.tips');
+whichTip.addEventListener("click", selectedTip);
 
-// function selectedTip(e) {
-//     if (e.target !== e.currentTarget) {
-//         const clickedTip = e.target.value;
-//     }
-// }
+function selectedTip(e) {
+    if (e.target !== e.currentTarget) {
+        const clickedTip = e.target.value;
+        alert(clickedTip)
+    }
+}
+// const calculate = document.querySelector('#calculate');
 
-const calculate = document.querySelector('#calculate');
-
-calculate.addEventListener('click', function () {
-
-    const billAmount = document.querySelector('#billInput').value;
-    const tipAmount = billAmount / 10;
-    alert(tipAmount)
-});
+// calculate.addEventListener('click', function () {
+//     const billAmount = document.querySelector('#billInput').value;
+//     const tipAmount = billAmount / clickedTip;
+//     alert(tipAmount)
+// });
