91313137859f067e17f49c072596e4e9fa444fca
nagysandordominik <nagysandordominik@icloud.com>
2021-07-21 21:01:07 +0200

    changed the custom tip button to 75% because i couldn't figure out how to select it yet

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const whichTip = document.querySelector('.tips');
-whichTip.addEventListener("click", selectedTip, false);
+whichTip.addEventListener("click", selectedTip);
 
 function selectedTip(e) {
     if (e.target !== e.currentTarget) {
         const clickedTip = e.target.value;
+        alert(clickedTip)
     }
 }
 
 const calculate = document.querySelector('#calculate');
 
 calculate.addEventListener('click', function () {
-    const clickedTip = whichTip;
     const billAmount = document.querySelector('#billInput').value;
     const tipAmount = billAmount / clickedTip;
     alert(tipAmount)
