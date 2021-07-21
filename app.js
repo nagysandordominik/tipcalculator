2dffb107e8f339de0da21674ec88c50bda960405
nagysandordominik <nagysandordominik@icloud.com>
2021-07-21 21:35:43 +0200

    tip selecting function works, calculating the tip amount works, but cannot call the clickedTip const from the first function

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const whichTip = document.querySelector('.tips');
-whichTip.addEventListener("click", selectedTip);
+// const whichTip = document.querySelector('.tips');
+// whichTip.addEventListener("click", selectedTip);
 
-function selectedTip(e) {
-    if (e.target !== e.currentTarget) {
-        const clickedTip = e.target.value;
-        alert(clickedTip)
-    }
-}
+// function selectedTip(e) {
+//     if (e.target !== e.currentTarget) {
+//         const clickedTip = e.target.value;
+//     }
+// }
 
 const calculate = document.querySelector('#calculate');
 
 calculate.addEventListener('click', function () {
+
     const billAmount = document.querySelector('#billInput').value;
-    const tipAmount = billAmount / clickedTip;
+    const tipAmount = billAmount / 10;
     alert(tipAmount)
 });
